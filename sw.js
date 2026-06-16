adffc37bee9691acc9d9839ed542ca5091f40a70
nay <chiranupong@gmail.com>
2026-06-16 10:50:33 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    SERVICE WORKER — NØYNAY PWA  v0.1.0
    ============================================= */
 
-const CACHE_NAME = 'noynay-v0.1.0';
+const CACHE_NAME = 'noynay-v0.1.1';
 
 const CACHE_FILES = [
   './',
